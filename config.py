27bd4eb2424edab43166784e72a5545e5aa40bd2
Dcbotzz <134841603+Dcbotzz@users.noreply.github.com>
2024-01-06 17:34:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,8 +37,8 @@ ADMINS = int(environ.get("ADMINS", ""))
 # Database Information
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "vjbotz")
+DB_URI = environ.get("DB_URI", "mongodb+srv://eternalbot:eF1cloYlSh1UDm9L@cluster0.iggjtiz.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = environ.get("DB_NAME", "eternalbot")
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
